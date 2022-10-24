d73ee2e50c2bdcaabce05da19f79330d002ff51b
michaelbulaongmusic <115378265+michaelbulaongmusic@users.noreply.github.com>
2022-10-24 23:11:32 +0800

    Update index.js
    simpler version

--- index.js
+++ index.js
@@ -1,52 +1,14 @@
 var randomNumber1 = (Math.floor(Math.random() * 6) + 1);
 var randomNumber2 = (Math.floor(Math.random() * 6) + 1);
 
+var randomDiceImage1 = "images/dice" + randomNumber1 + ".png";
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+
 var diceImage1 = document.querySelector("img.img1");
 var diceImage2 = document.querySelector("img.img2");
 
-
-// Roll Dice 1
-
-if (randomNumber1 === 1) {
-    diceImage1.setAttribute("src", "images/dice1.png");
-}
-else if (randomNumber1 === 2) {
-    diceImage1.setAttribute("src", "images/dice2.png");
-}
-else if (randomNumber1 === 3) {
-    diceImage1.setAttribute("src", "images/dice3.png");
-}
-else if (randomNumber1 === 4) {
-    diceImage1.setAttribute("src", "images/dice4.png");
-}
-else if (randomNumber1 === 5) {
-    diceImage1.setAttribute("src", "images/dice5.png");
-}
-else {
-    diceImage1.setAttribute("src", "images/dice6.png");
-}
-
-
-// Roll Dice 2
-
-if (randomNumber2 === 1) {
-    diceImage2.setAttribute("src", "images/dice1.png");
-}
-else if (randomNumber2 === 2) {
-    diceImage2.setAttribute("src", "images/dice2.png");
-}
-else if (randomNumber2 === 3) {
-    diceImage2.setAttribute("src", "images/dice3.png");
-}
-else if (randomNumber2 === 4) {
-    diceImage2.setAttribute("src", "images/dice4.png");
-}
-else if (randomNumber2 === 5) {
-    diceImage2.setAttribute("src", "images/dice5.png");
-}
-else {
-    diceImage2.setAttribute("src", "images/dice6.png");
-}
+diceImage1.setAttribute("src", randomDiceImage1);
+diceImage2.setAttribute("src", randomDiceImage2);
 
 
 // Who wins?
